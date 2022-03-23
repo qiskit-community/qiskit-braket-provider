9b615ce7da3d5622e7456e32e6924ee0d8d8ad70
Iskandar Sitdikov <iskandar.sitdikov@gmail.com>
2022-03-23 16:14:57 -0400

    Documentation: sphinx docs and action for GH Pages

--- qiskit_braket_plugin/providers/__init__.py
+++ qiskit_braket_plugin/providers/__init__.py
@@ -1,4 +1,24 @@
-"""Providers module."""
+"""
+=======================================================
+Provider module (:mod:`qiskit_braket_plugin.providers`)
+=======================================================
+
+.. currentmodule:: qiskit_braket_plugin.providers
+
+Provider module contains classes and functions to connect
+AWS Braket abstraction to Qiskit architecture.
+
+Provider classes and functions
+==============================
+
+.. autosummary::
+    :toctree: ../stubs/
+
+    AWSBraketBackend
+    BraketLocalBackend
+    AWSBraketProvider
+    AWSBraketJob
+"""
 
 from .braket_backend import AWSBraketBackend, BraketLocalBackend
 from .braket_provider import AWSBraketProvider
