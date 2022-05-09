7ae38f9fa32800efc5f58e660231da19464812da
Iskandar Sitdikov <iskandar.sitdikov@gmail.com>
2022-05-09 15:32:51 -0400

    Issue 4 | Tutorials: vqe on hybrid jobs

--- docs/tutorials/data/2_hybrid_jobs/job_script.py
+++ docs/tutorials/data/2_hybrid_jobs/job_script.py
@@ -10,18 +10,18 @@ from qiskit.algorithms.optimizers import SLSQP
 from qiskit.circuit.library import TwoLocal
 from qiskit.utils import QuantumInstance
 
-from qiskit_braket_provider import BraketLocalBackend
+from qiskit_braket_provider import AWSBraketProvider
 
 
 def main():
-    backend = BraketLocalBackend(name="default")
+    backend = AWSBraketProvider().get_backend("SV1")
 
     h2_op = (
-            (-1.052373245772859 * I ^ I)
-            + (0.39793742484318045 * I ^ Z)
-            + (-0.39793742484318045 * Z ^ I)
-            + (-0.01128010425623538 * Z ^ Z)
-            + (0.18093119978423156 * X ^ X)
+        (-1.052373245772859 * I ^ I)
+        + (0.39793742484318045 * I ^ Z)
+        + (-0.39793742484318045 * Z ^ I)
+        + (-0.01128010425623538 * Z ^ Z)
+        + (0.18093119978423156 * X ^ X)
     )
 
     quantum_instance = QuantumInstance(
@@ -34,12 +34,14 @@ def main():
 
     vqe_result = vqe.compute_minimum_eigenvalue(h2_op)
 
-    save_job_result({
-        "VQE": {
-            "eigenstate": vqe_result.eigenstate,
-            "eigenvalue": vqe_result.eigenvalue.real,
-            "optimal_parameters": list(vqe_result.optimal_parameters.values()),
-            "optimal_point": vqe_result.optimal_point.tolist(),
-            "optimal_value": vqe_result.optimal_value.real
+    save_job_result(
+        {
+            "VQE": {
+                "eigenstate": vqe_result.eigenstate,
+                "eigenvalue": vqe_result.eigenvalue.real,
+                "optimal_parameters": list(vqe_result.optimal_parameters.values()),
+                "optimal_point": vqe_result.optimal_point.tolist(),
+                "optimal_value": vqe_result.optimal_value.real,
+            }
         }
-    })
+    )
