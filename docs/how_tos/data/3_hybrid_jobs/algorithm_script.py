c6e548f3e7220716357cd11d902b60f16c32aa58
Iskandar Sitdikov <IceKhan13@users.noreply.github.com>
2022-05-09 16:18:18 -0400

    Issue #4 | hybrid jobs tutorial (#33)
    * Issue 4 | How-to: hybrid jobs
    * Issue 4 | Tutorials: vqe on hybrid jobs

---
+++ docs/how_tos/data/3_hybrid_jobs/algorithm_script.py
@@ -0,0 +1,17 @@
+"""Example of usage of Qiskit-Braket provider."""
+from qiskit import QuantumCircuit
+from qiskit_braket_provider import AWSBraketProvider
+
+from braket.jobs import save_job_result
+
+
+provider = AWSBraketProvider()
+backend = provider.get_backend("SV1")
+circuit = QuantumCircuit(2)
+circuit.h(0)
+circuit.cx(0, 1)
+
+results = backend.run(circuit, shots=1)
+
+print(results.result().get_counts())
+save_job_result(results.result().get_counts())
