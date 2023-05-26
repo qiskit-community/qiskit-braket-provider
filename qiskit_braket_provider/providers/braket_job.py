e1e4a4d6213c820e6acca4b88a71c3dc94cf583d
Ashmit JaiSarita Gupta <ashmitgupta.official@gmail.com>
2023-05-27 00:05:59 +0530

    Renamed AWSBraketJob to AmazonBraketTask

--- qiskit_braket_provider/providers/braket_job.py
+++ qiskit_braket_provider/providers/braket_job.py
@@ -65,8 +65,8 @@ def _get_result_from_aws_tasks(
     return experiment_results
 
 
-class AWSBraketJob(JobV1):
-    """AWSBraketJob."""
+class AmazonBraketTask(JobV1):
+    """AmazonBraketTask."""
 
     def __init__(
         self,
@@ -75,7 +75,7 @@ class AWSBraketJob(JobV1):
         tasks: Union[List[LocalQuantumTask], List[AwsQuantumTask]],
         **metadata: Optional[dict]
     ):
-        """AWSBraketJob for local execution of circuits.
+        """AmazonBraketTask for local execution of circuits.
 
         Args:
             job_id: id of the job
@@ -135,3 +135,5 @@ class AWSBraketJob(JobV1):
             status = JobStatus.RUNNING
 
         return status
+
+AWSBraketJob = AmazonBraketTask
