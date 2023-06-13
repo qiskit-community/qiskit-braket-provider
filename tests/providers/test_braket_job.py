ee16a98d1f3b1f76dcb77fe22aac50993bcd3d50
Memex <robotastray@cantab.net>
2023-06-13 19:42:30 +0100

    assert the job id in test_AWS_job() function in test_braket_job.py

--- tests/providers/test_braket_job.py
+++ tests/providers/test_braket_job.py
@@ -61,7 +61,8 @@ class TestAWSBraketJob(TestCase):
 
         self.assertTrue(isinstance(job, AWSBraketJob))
         self.assertEqual(job.shots, 10)
-
+        
+        self.assertEqual(job.result().job_id, "AwesomeId")
         self.assertEqual(job.status(), JobStatus.DONE)
 
     def test_AWS_result(self):
