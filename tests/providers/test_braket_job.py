c0416294dce11bd8480195b9e2ccc5f0ddc52436
WingCode <smallstar1234@gmail.com>
2024-06-01 11:56:00 +0200

    Fix pylint

--- tests/providers/test_braket_job.py
+++ tests/providers/test_braket_job.py
@@ -9,17 +9,21 @@ class TestAmazonBraketTask(TestCase):
     """Tests Amazon Braket Task"""
 
     def test_deprecation_warning_on_init(self):
+        """Test to check if a deprecation warning is raised when initializing AmazonBraketTask"""
         with self.assertWarns(DeprecationWarning):
 
-            class SubAmazonBraketTask(AmazonBraketTask):
-                pass
+            class SubAmazonBraketTask(
+                AmazonBraketTask
+            ):  # pylint: disable=unused-variable
+                """Subclass of AmazonBraketTask for testing"""
 
 
 class TestAWSBraketJob(TestCase):
     """Tests Amazon Braket Job"""
 
     def test_deprecation_warning_on_init(self):
+        """Test to check if a deprecation warning is raised when initializing AWSBraketJob"""
         with self.assertWarns(DeprecationWarning):
 
-            class SubAwsBraketJob(AWSBraketJob):
-                pass
+            class SubAwsBraketJob(AWSBraketJob):  # pylint: disable=unused-variable
+                """Subclass of AWSBraketJob for testing"""
