99bad325748c2830014964d745034ea767ee32e3
WingCode <smallstar1234@gmail.com>
2024-06-06 16:32:21 +0200

    Improve unit test coverage to 100% (#179)
    * Improve coverage to 100%
    
    * Improve coverage to 100%
    
    * Add tests
    
    * Improve coverage to 100%
    
    * Improve coverage to 100%
    
    * Improve coverage to 100%
    
    * Fix region retrieval
    
    * Lint import
    
    * Fix black
    
    * Fix pylint
    
    * Fix pylint
    
    * Fix pylint
    
    * Fix pylint
    
    * Fix pylint
    
    * Bump code coverage condition
    
    * improve tests
    
    * improve tests

---
+++ tests/providers/test_braket_job.py
@@ -0,0 +1,29 @@
+"""Tests for Braket jobs"""
+
+from unittest import TestCase
+
+from qiskit_braket_provider.providers.braket_job import AmazonBraketTask, AWSBraketJob
+
+
+class TestAmazonBraketTask(TestCase):
+    """Tests Amazon Braket Task"""
+
+    def test_deprecation_warning_on_init(self):
+        """Test to check if a deprecation warning is raised when initializing AmazonBraketTask"""
+        with self.assertWarns(DeprecationWarning):
+
+            class SubAmazonBraketTask(
+                AmazonBraketTask
+            ):  # pylint: disable=unused-variable
+                """Subclass of AmazonBraketTask for testing"""
+
+
+class TestAWSBraketJob(TestCase):
+    """Tests Amazon Braket Job"""
+
+    def test_deprecation_warning_on_init(self):
+        """Test to check if a deprecation warning is raised when initializing AWSBraketJob"""
+        with self.assertWarns(DeprecationWarning):
+
+            class SubAwsBraketJob(AWSBraketJob):  # pylint: disable=unused-variable
+                """Subclass of AWSBraketJob for testing"""
