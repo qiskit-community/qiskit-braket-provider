cdf79b6c6b1911e91f2149cd50d83a517d491f65
Cody Wang <speller26@gmail.com>
2025-11-21 13:16:36 -0800

    Update braket_primitive_task.py

--- qiskit_braket_provider/providers/braket_primitive_task.py
+++ qiskit_braket_provider/providers/braket_primitive_task.py
@@ -34,72 +34,29 @@ class BraketPrimitiveTask(BasePrimitiveJob[PrimitiveResult[PubResult], JobStatus
         self._result = None
 
     def result(self) -> PrimitiveResult:
-        """
-        Get the result of the task.
-
-        Returns:
-            PrimitiveResult: PrimitiveResult containing PubResult for each pub.
-        """
         if self._result is None:
             self._result = self._result_translator(self._task.result())
         return self._result
 
     def status(self) -> JobStatus:
-        """
-        Get the status of the task.
-
-        Returns:
-            JobStatus: Job status string.
-        """
         return self._get_task_status()
 
     def cancel(self):
-        """Cancel the task."""
         self._task.cancel()
 
     def job_id(self) -> str:
-        """
-        Get the task ID.
-
-        Returns:
-            str: Task ID string.
-        """
         return self._task.id
 
     def done(self) -> bool:
-        """
-        Check if the task is done.
-
-        Returns:
-            bool: True if the task is done, False otherwise.
-        """
         return self._get_task_status() == JobStatus.DONE
 
     def running(self) -> bool:
-        """
-        Check if the task is running.
-
-        Returns:
-            bool: True if the task is running, False otherwise.
-        """
         return self._get_task_status() == JobStatus.RUNNING
 
     def cancelled(self) -> bool:
-        """
-        Check if the task was cancelled.
-
-        Returns:
-            bool: True if the task was cancelled, False otherwise.
-        """
         return self._get_task_status() == JobStatus.CANCELLED
 
     def in_final_state(self) -> bool:
-        """
-        Check if the task is in a final state.
-
-        Returns:
-            bool: True if the task is in a final state, False otherwise.
-        """
         return self._get_task_status() in [JobStatus.DONE, JobStatus.ERROR, JobStatus.CANCELLED]
 
     def _get_task_status(self) -> JobStatus:
