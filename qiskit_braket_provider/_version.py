48435668553de70d3c3506be2613a7e185ab713d
ci <ci>
2026-04-23 21:19:54 +0000

    prepare release v0.13.1

--- qiskit_braket_provider/_version.py
+++ qiskit_braket_provider/_version.py
@@ -11,4 +11,4 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
-__version__ = "0.13.1.dev0"
+__version__ = "0.13.1"
